a57c0bfeb83304aa4f9d6f8df0566041c2df0c86
J.V.lakshmi <jujjuvarapuvenkatalakshmi@gmail.com>
2026-07-28 10:44:46 +0000

    js

---
+++ js/script.js
@@ -0,0 +1,4 @@
+alert("Welcome to NRIIT Learning Management System")
+let heading = document.getElementById("Welcome")
+heading.innerHTML = "Welcome Future software Engineers"
+console.log("Heading elements: ",heading)
