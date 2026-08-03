f58d974034b32b2f172386153857eaeb205caea0
J.V.lakshmi <jujjuvarapuvenkatalakshmi@gmail.com>
2026-08-03 09:41:46 +0000

    startend flask

--- README.md
+++ README.md
@@ -9,3 +9,14 @@ Flexbox
 Grid
 Responsive Design
 Bootstrap (Introduction)
+
+DNS-Domani Name Server
+api-appilication programming interface->stands between frontend and backend
+middleware - stands as security check for request and response
+Authorization vs authentication
+authentication tells who are you eg: normal login
+authentication tells what you can able to do eg:staff and students
+react- web framework for frontend development in javascript
+flask,fastapi,django-web framework for backend development in python
+orm- object relational mapping, it is a technique that allows you to query and manipulate data from a database.
+
--- js/test.js
+++ js/test.js
@@ -1,33 +1,53 @@
-console.log("Hello World")
-let student_name = "Venkata Lakshmi"
-console.log("Student name is:"+ student_name)
-let student_age = 21
-let course_name = "Python Full Stack"
-console.log("Student age is :"+ student_age,"course name is:"+course_name)
-let fee = 10000
-let discount = 10
-let discounted_fee = fee - (fee *discount / 100)
-console.log("discounted fee is:"+discounted_fee)
-let age=18
-if(age>=18){
-    console.log("Eligible for admission")
+// console.log("Hello World")
+// let student_name = "Venkata Lakshmi"
+// console.log("Student name is:"+ student_name)
+// let student_age = 21
+// let course_name = "Python Full Stack"
+// console.log("Student age is :"+ student_age,"course name is:"+course_name)
+// let fee = 10000
+// let discount = 10
+// let discounted_fee = fee - (fee *discount / 100)
+// console.log("discounted fee is:"+discounted_fee)
+// let age=18
+// if(age>=18){
+//     console.log("Eligible for admission")
+// }
+// else{
+//     console.log("Not Eligible for admission")
+// }
+// for(let i=1; i<=5; i++){
+//     console.log("Iteration number:" +=i)
+// }
+// const pi = 3.14
+// console.log("value of pi is:" + pi)
+// let student = {
+//     Name: "Venkata Lakshmi",
+//     age: 21,
+//     course: "Python Full Stack"
+//     fee = 10000
+// }
+// console.log("Student details: ",student)
+// function greetStudent(name){
+//     console.log("Hello " + name + ",Welcome to NRIIT Learning Management System")
+// }
+// greetStudent(student_name)
+
+// write a function to square a number
+// write a funtion to odd or even number
+function squareNumber(num) {
+    return num*num;
+
+
 }
-else{
-    console.log("Not Eligible for admission")
+    x=squareNumber(5);
+    console.log("square of 5 is: ",x)
+    //write a funtion to odd or even number
+    function checkOddEven(num) {
+        if (num % 2 === 0) {
+            return "Even";
+            
+    } else {
+        return "Odd";
+    }
 }
-for(let i=1; i<=5; i++){
-    console.log("Iteration number:" +=i)
-}
-const pi = 3.14
-console.log("value of pi is:" + pi)
-let student = {
-    Name: "Venkata Lakshmi",
-    age: 21,
-    course: "Python Full Stack"
-    fee = 10000
-}
-console.log("Student details: ",student)
-function greetStudent(name){
-    console.log("Hello " + name + ",Welcome to NRIIT Learning Management System")
-}
-greetStudent(student_name)
+console.log("Check if 7 is odd or even: ", checkOddEven(7));
