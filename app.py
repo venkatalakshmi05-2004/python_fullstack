71bb6e96ecaf5b96db13d14d3766a6ae29aab742
J.V.lakshmi <jujjuvarapuvenkatalakshmi@gmail.com>
2026-08-18 09:20:19 +0000

    me

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import sqlite3
 # pyrefly: ignore [missing-import]
 from flask import Flask,render_template,jsonify,request,redirect,url_for,session
+# pyrefly: ignore [missing-import]
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.secret_key="super_secret_key"
@@ -78,8 +80,9 @@ def api_register():
     user = cursor.fetchone()
     if user:
         return jsonify({"status": "error", "message": "User already exists with this email!"}), 400
+    hashed_password =generate_password_hash(data["password"])
     
-    cursor.execute("INSERT INTO users (name, email, password, dob, gender, course) VALUES (?, ?, ?, ?, ?, ?)", (data["name"], data["email"], data["password"], data["dob"], data["gender"], data["course"]))
+    cursor.execute("INSERT INTO users (name, email, password, dob, gender, course) VALUES (?, ?, ?, ?, ?, ?)", (data["name"], data["email"], hashed_password, data["dob"], data["gender"], data["course"]))
     conn.commit()
     conn.close()
     return jsonify({"status": "success", "message": "Registration successful!"})
@@ -94,7 +97,7 @@ def api_login():
     cursor.execute("SELECT * FROM users WHERE email = ?", (email,))
     user = cursor.fetchone()
     conn.close()
-    if user and user["password"] == password:
+    if user and user and check_password_hash(user ["password"], password):
         # session variables to keep track of logged in user
         session["user_email"] = user["email"]
         session["user_name"] = user["name"]
